d8560e54858dc70a14aeb69b582f0319ddd3e0f3
akshit-sharma <1ashkit@gmail.com>
2015-08-18 01:01:47 +0530

    Mail script added.

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+
+    $hello = 'hello world';
+    echo $hello;
+
     $to = $_REQUEST['to'];
     $subject = 'Test mail';
     $message = "this is a test message";
     mail ($to, $subject, $message);
+
+
 ?>
