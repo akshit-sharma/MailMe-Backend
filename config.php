eb1760890af6a6396be96bd0b151f9163839fd08
akshit-sharma <1ashkit@gmail.com>
2015-08-18 01:18:00 +0530

    Have to secure config file.

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+    $debug = true;
+
+    $allowed_domains = array('','');
+
+    $REFERRER = $_SERVER['HTTP_REFERER'];
+
+    if($debug)
+        echo "Referrer is $REFERRER";
+
+    if ($REFERRER == '') {
+        exit(header('Location: 404.php'));
+    }
+
+    $domain = substr($REFERRER, strpos($REFERRER, '://')+3);
+    $domain = substr($domain, 0, strpos($domain, '/'));
+
+    if (!in_array($domain, $allowed_domains)) {
+        exit(header('Location:404.php'));
+    }
+
+?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     $subject = 'Test mail';
     $message = "this is a test message";
     if(isset($to)){
-        echo "Sending mail to $to";
+        echo "Sending mail to $to<br/>";
         if( mail ($to, $subject, $message) ){
             echo "Mail sent<br/>";
         }else{
