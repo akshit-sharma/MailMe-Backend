c4d013fe4b4982ac67106bf07f9e1286252b5874
akshit-sharma <1ashkit@gmail.com>
2015-08-18 01:23:05 +0530

    Config file according to SendGrid.

--- config.php
+++ config.php
@@ -1,26 +1,9 @@
 <?php
 
-    $debug = true;
+    class SendGrid{
 
-    $allowed_domains = array('','');
-
-    $REFERRER = $_SERVER['HTTP_REFERER'];
-
-    if($debug){
-        echo "Referrer is $REFERRER";
-    }
-    else{
-
-        if ($REFERRER == '') {
-            exit(header('Location: 404.php'));
-        }
-
-        $domain = substr($REFERRER, strpos($REFERRER, '://')+3);
-        $domain = substr($domain, 0, strpos($domain, '/'));
-
-        if (!in_array($domain, $allowed_domains)) {
-            exit(header('Location:404.php'));
-        }
+        $api_user = "< ENTER SENDGRID user HERE >";
+        $api_key = "< ENTER SENDGRID key HERE >";
 
     }
 
