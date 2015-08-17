3a37df93df2f303cb7c339e96e46fe863bcda224
akshit-sharma <1ashkit@gmail.com>
2015-08-18 02:28:46 +0530

    composer added

--- configfile.php
+++ configfile.php
@@ -5,7 +5,7 @@
     *
     */
 
-    class SendGrid{
+    class SendGridConfig{
 
         $api_user = "< ENTER SENDGRID user HERE >";
         $api_key = "< ENTER SENDGRID key HERE >";
--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <?php
 
+    require_once('config.php');
+
     $hello = 'hello world<br/>';
     echo $hello;
 
     $to = $_REQUEST['to'];
     $subject = 'Test mail';
     $message = "this is a test message";
-    if(isset($to)){
-        echo "Sending mail to $to<br/>";
-        if( mail ($to, $subject, $message) ){
-            echo "Mail sent<br/>";
-        }else{
-            echo "Some error occured<br/>";
-        }
-    }
+    $from = "faaltu@faaltu.lol";
+
+    $sendgridconfig = new SendGridConfig();
+
+    $api_user  = $sendgridconfig->$api_user;
+    $api_key  = $sendgridconfig->$api_key;
+
+    $sendgrid = new SendGrid($api_user, $api_key);
+    $email    = new SendGrid\Email();
+
+    $email->addTo($to)
+          ->setFrom($from)
+          ->setSubject($subject)
+          ->setHtml($message);
+
+    $sendgrid->send($email);
 
 ?>
